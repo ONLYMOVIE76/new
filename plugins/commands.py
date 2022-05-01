e18344689ea1f9b4f6fec0080f43ef05b166a35c
Rahul Rahaman <rahulrahaman484@gmail.com>
2022-05-01 15:42:38 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -41,11 +41,6 @@ async def start(client, message):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
-        buttons = [[
-            InlineKeyboardButton('📢 Support Channel', url=f'https://t.me/iPrimeHub'),
-            InlineKeyboardButton('➕ Add Me', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
